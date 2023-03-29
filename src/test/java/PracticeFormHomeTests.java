d69766fc980e787506f3766c6fcf7939de639116
Zalina Kodz <kod3ik87@gmail.com>
2023-03-29 12:04:44 +0300

    added fixes4

--- src/test/java/PracticeFormHomeTests.java
+++ src/test/java/PracticeFormHomeTests.java
@@ -5,40 +5,41 @@ import pages.components.RegistrationFinalModal;
 
 
 public class PracticeFormHomeTests extends TestBase {
-    public RegistrationPage registrationPage = new RegistrationPage();
-    public RegistrationFinalModal registrationFinalModal = new RegistrationFinalModal();
+ RegistrationPage registrationPage = new RegistrationPage();
+ RegistrationFinalModal registrationFinalModal = new RegistrationFinalModal();
+UserDataDetailsForForm userDataDetailsForForm = new UserDataDetailsForForm();
 
     @Test
     void fillFormTest() {
 
         registrationPage.openPage()
                 .removeBanner()
-                .setFirstName(UserDataDetailsForForm.firstName)
-                .setLastName(UserDataDetailsForForm.lastName)
-                .setUserEmail(UserDataDetailsForForm.userEmail)
-                .setGender(UserDataDetailsForForm.userGender)
-                .setPhoneNumber(UserDataDetailsForForm.userMobilePhone)
-                .setBirthDay(UserDataDetailsForForm.dayOfBirth, UserDataDetailsForForm.monthOfBirth, UserDataDetailsForForm.yearOfBirth)
-                .setSubject(UserDataDetailsForForm.userSubject)
-                .chooseHobby(UserDataDetailsForForm.userHobby)
-                .uploadPicture(UserDataDetailsForForm.userPicture)
-                .setAddress(UserDataDetailsForForm.userAddress)
+                .setFirstName(userDataDetailsForForm.firstName)
+                .setLastName(userDataDetailsForForm.lastName)
+                .setUserEmail(userDataDetailsForForm.userEmail)
+                .setGender(userDataDetailsForForm.userGender)
+                .setPhoneNumber(userDataDetailsForForm.userMobilePhone)
+                .setBirthDay(userDataDetailsForForm.dayOfBirth, userDataDetailsForForm.monthOfBirth, userDataDetailsForForm.yearOfBirth)
+                .setSubject(userDataDetailsForForm.userSubject)
+                .chooseHobby(userDataDetailsForForm.userHobby)
+                .uploadPicture(userDataDetailsForForm.userFilePath)
+                .setAddress(userDataDetailsForForm.userAddress)
                 .scrollPageIntoView(true)
-                .selectState(UserDataDetailsForForm.userState)
-                .selectCity(UserDataDetailsForForm.userCity)
+                .selectState(userDataDetailsForForm.userState)
+                .selectCity(userDataDetailsForForm.userCity)
                 .submit();
 
         registrationFinalModal.verifyModalDetails()
-                .verifyModalResult("Student Name", UserDataDetailsForForm.firstName + ' ' + UserDataDetailsForForm.lastName)
-                .verifyModalResult("Student Email", UserDataDetailsForForm.userEmail)
-                .verifyModalResult("Gender", UserDataDetailsForForm.userGender)
-                .verifyModalResult("Mobile", UserDataDetailsForForm.userMobilePhone)
-                .verifyModalResult("Date of Birth", UserDataDetailsForForm.dayOfBirth + " " + UserDataDetailsForForm.monthOfBirth + "," + UserDataDetailsForForm.yearOfBirth)
-                .verifyModalResult("Subjects", UserDataDetailsForForm.userSubject)
-                .verifyModalResult("Hobbies", UserDataDetailsForForm.userHobby)
+                .verifyModalResult("Student Name", userDataDetailsForForm.firstName + ' ' + userDataDetailsForForm.lastName)
+                .verifyModalResult("Student Email", userDataDetailsForForm.userEmail)
+                .verifyModalResult("Gender", userDataDetailsForForm.userGender)
+                .verifyModalResult("Mobile", userDataDetailsForForm.userMobilePhone)
+                .verifyModalResult("Date of Birth", userDataDetailsForForm.dayOfBirth + " " + userDataDetailsForForm.monthOfBirth + "," + userDataDetailsForForm.yearOfBirth)
+                .verifyModalResult("Subjects", userDataDetailsForForm.userSubject)
+                .verifyModalResult("Hobbies", userDataDetailsForForm.userHobby)
                 .verifyModalResult("Picture", "photo_2023-02-24_16-16-09.jpg")
-                .verifyModalResult("Address", UserDataDetailsForForm.userAddress)
-                .verifyModalResult("State and City", UserDataDetailsForForm.userState + " " + UserDataDetailsForForm.userCity);
+                .verifyModalResult("Address", userDataDetailsForForm.userAddress)
+                .verifyModalResult("State and City", userDataDetailsForForm.userState + " " + userDataDetailsForForm.userCity);
 
     }
 }
--- src/test/java/UserDataDetailsForForm.java
+++ src/test/java/UserDataDetailsForForm.java
@@ -1,4 +1,6 @@
+
 import com.github.javafaker.Faker;
+
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,38 +9,42 @@ import java.util.Map;
 
 public class UserDataDetailsForForm {
 
-    public static String[] subjects = {"Accounting", "Maths", "Arts",
+    Faker faker = new Faker();
+    String userFilePath = "src/test/resources/photo_2023-02-24_16-16-09.jpg";
+
+    String[] subjects = new String[]{"Accounting", "Maths", "Arts",
             "English", "Physics", "Chemistry", "Computer Science",
             "Economics", "Social Studies", "History", "Civics", "Commerce",
             "Hindi", "Biology"};
-    public static String[] hobby = {"Reading", "Sports", "Music"};
-    public static Map<String, String[]> statesAndCities = Map.of(
+    String[] hobbies = new String[]{"Reading", "Sports", "Music"};
+
+
+
+    Map<String, String[]> statesAndCities = Map.of(
             "NCR", new String[]{"Delhi", "Gurgaon", "Noida"},
             "Uttar Pradesh", new String[]{"Agra", "Lucknow", "Merrut"},
             "Haryana", new String[]{"Karnal", "Panipat"},
             "Rajasthan", new String[]{"Jaipur", "Jaiselmer"});
-    public static String userFilePath = "src/test/resources/photo_2023-02-24_16-16-09.jpg";
 
 
-    static Faker faker = new Faker();
 
-    static Date dateOfBirth = faker.date().birthday();
-    public  static String dayOfBirth = (new SimpleDateFormat("dd", Locale.ENGLISH)).format(dateOfBirth),
-            monthOfBirth = (new SimpleDateFormat("MMMM", Locale.ENGLISH)).format(dateOfBirth),
-            yearOfBirth = (new SimpleDateFormat("y", Locale.ENGLISH)).format(dateOfBirth);
+       Date dateOfBirth = faker.date().birthday();
+       public String dayOfBirth = (new SimpleDateFormat("dd", Locale.ENGLISH)).format(dateOfBirth),
+                monthOfBirth = (new SimpleDateFormat("MMMM", Locale.ENGLISH)).format(dateOfBirth),
+                yearOfBirth = (new SimpleDateFormat("y", Locale.ENGLISH)).format(dateOfBirth);
 
 
-    static String firstName = faker.name().firstName(),
-            lastName = faker.name().lastName(),
-            userEmail = faker.internet().emailAddress(),
-            userGender = faker.demographic().sex(),
-            userMobilePhone = 8 + faker.phoneNumber().subscriberNumber(9),
-            userSubject = faker.options().option(UserDataDetailsForForm.subjects),
-            userPicture = UserDataDetailsForForm.userFilePath,
-            userHobby = faker.options().option(UserDataDetailsForForm.hobby),
-            userAddress = faker.address().fullAddress(),
-            userState = faker.options().option(UserDataDetailsForForm.statesAndCities.keySet().toArray()).toString(),
-            userCity = faker.options().option(UserDataDetailsForForm.statesAndCities.get(userState));
+    public String
+        firstName = faker.name().firstName(),
+        lastName = faker.name().lastName(),
+        userEmail = faker.internet().emailAddress(),
+        userGender = faker.demographic().sex(),
+        userMobilePhone = 8 + faker.phoneNumber().subscriberNumber(9),
+        userSubject = faker.options().option(subjects),
+        userHobby = faker.options().option(hobbies),
+        userAddress = faker.address().fullAddress(),
+        userState = faker.options().option(statesAndCities.keySet().toArray()).toString(),
+        userCity = faker.options().option(statesAndCities.get(userState));
 
 
 }
